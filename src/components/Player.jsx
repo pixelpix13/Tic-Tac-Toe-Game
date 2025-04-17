f1f6bb0ce8b1cea02f5362d38a3a756df61060ac
pixelpix13 <pixelpix99@gmail.com>
2025-04-16 23:27:12 -0500

    Added Player Component

---
+++ src/components/Player.jsx
@@ -0,0 +1,19 @@
+import {useState} from "react";
+
+function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const handleEditClick = () => {
+        setIsEditing(editing=>!editing);
+    };
+    return (
+        <li>
+        <span className="player">
+            {isEditing?<input type="text" required value={name}/>:<span className="player-name">{name}</span>}
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing?"Save":"Edit"}</button>
+        </li>
+    );
+};
+
+export default Player;
